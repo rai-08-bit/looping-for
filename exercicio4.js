7bd523955694b6c08b9874d59dc7b639bfe84d5a
rai-08-bit <raissa.bez08@outlook.com>
2025-05-05 17:31:08 -0300

    Update exercicio4.js

--- exercicio4.js
+++ exercicio4.js
@@ -1,12 +1,12 @@
-let soma = 0;// criação da variável "soma" e atribuição de valor 0
+let soma = 0;// criação da variável "soma" e atribuição de valor igual a 0
 for (let i = 1; i <= 5; i++) {
-    const num = parseFloat(prompt(`digite a nota ${i}`)) //
+    const num = parseFloat(prompt(`Digite a nota da atividade ${i}`)) // Criação da constante "num" 
 
     if (isNaN(num) || num < 0) {
-        alert("Por favor, digite uma nota válido.");// 
+        alert("Você não digitou um número válido, tente novamente");// Verificação de valores da variável "num"
     } else {
-        soma += num;//
+        soma += num;
     }
 }
-let media = soma / 5;//
-alert(`a média das notas digitadas foram: ${media} na soma das notas digitadas foram: ${soma}`);//
+let media=soma /5; // Criação da variável "media" e atribuição de valores à ela, tendo seu resultado como a divisão do resultado do valor da variável "soma" por 5  
+alert(`A média é: ${media}`);
